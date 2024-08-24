0c262fb5a490d41a2b41f51d5ea02c3acd5a7d55
shipcommit <fredrik.a@protonmail.ch>
2024-08-24 12:34:02 +0200

    Update

--- app/components/ArticleRewriter.js
+++ app/components/ArticleRewriter.js
@@ -7,28 +7,21 @@ import Form from './Form';
 import content from '@/data/content';
 
 export default function ArticleRewriter() {
-  const [text, setText] = useState('');
-  // const [rewrittenText, setRewrittenText] = useState(null);
-  const [formData, setFormData] = useState(null);
-
-  useEffect(() => {
-    // Set the initial text to the content from content.js
-    setText(content);
-  }, []);
+  const [formResponse, setFormResponse] = useState(null);
 
   const handleFormSubmit = (data) => {
-    setFormData(data);
-
-    console.log('formData ArticleRewriter.js:', formData);
-    // You can also update rewrittenText here if needed
-    // setRewrittenText(data.someProperty);
+    setFormResponse(data);
   };
 
   return (
     <div className="grid grid-cols-1 gap-4 items-center w-full max-w-2xl mx-auto">
       <Form className="w-full" onSubmit={handleFormSubmit} />
       <div className="mt-4">
-        {formData ? <Article data={formData} /> : <Article data={text} />}
+        {formResponse ? (
+          <Article data={formResponse} />
+        ) : (
+          <Article data={content} />
+        )}
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import ArticleRewriter from './components/ArticleRewriter';
 export default function Home() {
   return (
     <div className="flex flex-col items-center py-8 w-full">
-      <div className="prose ">
+      <div className="prose">
         <ArticleRewriter />
       </div>
     </div>
